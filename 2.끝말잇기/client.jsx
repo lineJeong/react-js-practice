d2e3f5a4d5eec7d80f266da0237ec7402841e506
lineJeong <line.jeong32@gmail.com>
2022-12-31 23:30:08 +0900

    2-7. @babel/preset-env와 plugins

--- 2.끝말잇기/client.jsx
+++ 2.끝말잇기/client.jsx
@@ -1,6 +1,8 @@
 const React = require("react");
-const ReactDom = require("react-dom");
+// const ReactDom = require("react-dom");
+const ReactDom = require("react-dom/client");
 
 const WordRelay = require("./WordRelay");
 
-ReactDom.render(<WordRelay />, document.querySelector("#root"));
+// ReactDom.render(<WordRelay />, document.querySelector("#root"));
+ReactDom.createRoot(document.querySelector("#root")).render(<WordRelay />);
--- 2.끝말잇기/webpack.config.js
+++ 2.끝말잇기/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   name: "wordrelay-setting",
@@ -11,20 +12,30 @@ module.exports = {
   entry: {
     app: ["./client"],
   },
-
   module: {
     rules: [
       {
         test: /\.jsx?/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 5% in KR"],
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
           plugins: ["@babel/plugin-proposal-class-properties"],
         },
       },
     ],
   },
-
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
   output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js",
