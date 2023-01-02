7da9e674749dcefdf74ebfb8bb0b172a3652f340
lineJeong <line.jeong32@gmail.com>
2023-01-02 14:48:03 +0900

    컨트롤드 인풋 vs 언컨트롤드 인풋

--- 2.끝말잇기/WordRelay-hooks.jsx
+++ 2.끝말잇기/WordRelay-hooks.jsx
@@ -3,40 +3,44 @@ const { useState, useRef } = React;
 
 const WordRelayHooks = () => {
   const [word, setWord] = useState("제시어");
-  const [value, setValue] = useState("");
+  // const [value, setValue] = useState("");
   const [result, setResult] = useState("");
   const inputRef = useRef(null);
 
   const onSubmitForm = (e) => {
     e.preventDefault();
-    if (word[word.length - 1] === value[0]) {
-      setWord(value);
+    if (word[word.length - 1] === e.target.children.wordInput.value[0]) {
+      // setWord(value);
+      setWord(e.target.children.wordInput.value);
       setResult("딩동댕");
-      setValue("");
     } else {
       setResult("땡");
-      setValue("");
     }
+    // setValue("");
+    e.target.children.wordInput.value = "";
     inputRef.current.focus();
   };
 
-  const onChangeInput = (e) => {
-    setValue(e.currentTarget.value);
-  };
+  // const onChangeInput = (e) => {
+  //   setValue(e.currentTarget.value);
+  // };
 
   return (
     <>
       <div>{word}</div>
       <form onSubmit={onSubmitForm}>
         <label htmlFor="wordInput">글자를 입력하세요.</label>
-        <input
+        {/* 언컨트롤드 인풋 */}
+        <input id="wordInput" ref={inputRef} type="text" />
+        {/* 컨트롤드 인풋 */}
+        {/* <input
           id="wordInput"
           className="wordInput"
           ref={inputRef}
           type="text"
           value={value}
           onChange={onChangeInput}
-        />
+        /> */}
         <button>입력</button>
       </form>
       <div>{result}</div>
