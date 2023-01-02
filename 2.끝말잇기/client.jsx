231129f732f1c0df9e4de4ddb0084b93f4344807
lineJeong <line.jeong32@gmail.com>
2023-01-02 10:18:30 +0900

    2-10. 끝말잇기 Hooks로 전환하기

--- 2.끝말잇기/client.jsx
+++ 2.끝말잇기/client.jsx
@@ -2,7 +2,8 @@ const React = require("react");
 // const ReactDom = require("react-dom");
 const ReactDom = require("react-dom/client");
 
-const WordRelay = require("./WordRelay");
+// const WordRelay = require("./WordRelay");
+const WordRelay = require("./WordRelay-hooks");
 
 // ReactDom.render(<WordRelay />, document.querySelector("#root"));
 ReactDom.createRoot(document.querySelector("#root")).render(<WordRelay />);
