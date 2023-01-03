f03a6fb336ec079062368a35e55830eabcc1bdb9
lineJeong <line.jeong32@gmail.com>
2023-01-03 17:33:24 +0900

    3-2. 리액트 반복문(key)

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -1,9 +1,47 @@
-import React, { Component } from "react";
+import { Component } from "react";
+
+function getNumbers() {
+  // 숫자 4개를 랜덤하게 뽑는 함수 (중복X)
+}
 
 class NumberBaseball extends Component {
-  state = {};
+  state = {
+    value: "",
+    result: "",
+    answer: getNumbers(),
+    tries: 0,
+  };
 
-  render() {}
+  onSubmitForm = (e) => {
+    e.preventDefault();
+  };
+
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            type="text"
+            maxLength={4}
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+          <button>입력</button>
+        </form>
+        <div>시도: {this.state.tries.length}</div>
+        <ul>
+          {[1, 2, 3].map((el) => (
+            <li>{el}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default NumberBaseball;
