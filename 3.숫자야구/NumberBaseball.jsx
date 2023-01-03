ef6a3c04abe2b9974a9eafb96215f495600ac5ac
lineJeong <line.jeong32@gmail.com>
2023-01-03 14:24:15 +0900

    3-1. import와 require 비교

---
+++ 3.숫자야구/NumberBaseball.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class NumberBaseball extends Component {
+  state = {};
+
+  render() {}
+}
+
+export default NumberBaseball;
+
+// module.exports NumberBaseball === export default NumberBaseball
+// exports.hello = "hello" === export const hello = "hello"
+
+// module.exports = { hello: "a" } === exports.hello = "a"
