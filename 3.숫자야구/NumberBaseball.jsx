fc2b31f26cbc855c78b2f41f2cc2686d3bbbc8fe
lineJeong <line.jeong32@gmail.com>
2023-01-08 20:34:59 +0900

    숫자야구 Hooks로 전환하기(+useState lazy init)

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -28,12 +28,14 @@ class NumberBaseball extends Component {
   onSubmitForm(e) {
     e.preventDefault();
     if (this.state.value === this.state.answer.join("")) {
-      this.setState({
-        result: "홈런",
-        tries: [
-          ...this.state.tries,
-          { try: this.state.value, result: "홈런!" },
-        ],
+      this.setState((prevState) => {
+        return {
+          result: "홈런",
+          tries: [
+            ...prevState.tries,
+            { try: this.state.value, result: "홈런!" },
+          ],
+        };
       });
       alert("게임을 다시 시작합니다.");
       this.setState({
@@ -63,14 +65,16 @@ class NumberBaseball extends Component {
             ball += 1;
           }
         }
-        this.setState({
-          tries: [
-            ...this.state.tries,
-            {
-              try: this.state.value,
-              result: `${strike} 스트라이크, ${ball} 볼입니다.`,
-            },
-          ],
+        this.setState((prevState) => {
+          return {
+            tries: [
+              ...prevState.tries,
+              {
+                try: this.state.value,
+                result: `${strike} 스트라이크, ${ball} 볼입니다.`,
+              },
+            ],
+          };
         });
       }
     }
--- 3.숫자야구/client.jsx
+++ 3.숫자야구/client.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-import NumberBaseball from "./NumberBaseball";
+import NumberBaseball from "./NumberBaseball-Hooks";
 
 ReactDom.createRoot(document.querySelector("#root")).render(<NumberBaseball />);
