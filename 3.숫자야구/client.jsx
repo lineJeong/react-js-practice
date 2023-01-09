e7ad35ae45c509e4984bc92e81821eeb146d7007
lineJeong <line.jeong32@gmail.com>
2023-01-09 17:51:08 +0900

    3-10. shouldComponentUpdate

--- 3.숫자야구/client.jsx
+++ 3.숫자야구/client.jsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 import NumberBaseball from "./NumberBaseball-Hooks";
+import RenderTest from "./renderTest";
 
-ReactDom.createRoot(document.querySelector("#root")).render(<NumberBaseball />);
+ReactDom.createRoot(document.querySelector("#root")).render(<RenderTest />);
