9ddcf23974a8542c6c51aa3e75f392fbebdac9b2
lineJeong <line.jeong32@gmail.com>
2023-01-11 23:52:39 +0900

    3-13. props와 state 연결하기

--- 3.숫자야구/Try-Hooks.jsx
+++ 3.숫자야구/Try-Hooks.jsx
@@ -1,10 +1,16 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 
 const Try = memo(({ tryInfo }) => {
+  const [result, setResult] = useState(tryInfo.result);
+
+  const onClick = () => {
+    setResult("Try 컴포넌트에서 result 변경");
+  };
+
   return (
     <li>
       <div>{tryInfo.try}</div>
-      <div>{tryInfo.result}</div>
+      <div onClick={onClick}>{result}</div>
     </li>
   );
 });
--- 3.숫자야구/Try.jsx
+++ 3.숫자야구/Try.jsx
@@ -1,11 +1,17 @@
 import React, { PureComponent } from "react";
 
 class Try extends PureComponent {
+  state = {
+    result: this.props.result,
+    try: this.props.try,
+  };
+
   render() {
+    const { tryInfo } = this.props;
     return (
       <li>
-        <div>{this.props.tryInfo.try}</div>
-        <div>{this.props.tryInfo.result}</div>
+        <div>{tryInfo.try}</div>
+        <div>{tryInfo.result}</div>
       </li>
     );
   }
