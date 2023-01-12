fb783c591feaaf6264bdb4e713a8d7093e28119c
lineJeong <line.jeong32@gmail.com>
2023-01-12 19:11:29 +0900

    4-1. React 조건문

---
+++ 4.반응속도체크/client.jsx
@@ -0,0 +1,7 @@
+const React = require("react");
+const ReactDom = require("react-dom/client");
+
+// const ResponseCheck = require("./ResponseCheck");
+import ResponseCheck from "./ResponseCheck";
+
+ReactDom.createRoot(document.querySelector("#root")).render(<ResponseCheck />);
