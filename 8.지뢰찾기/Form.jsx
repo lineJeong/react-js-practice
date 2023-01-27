25babfe3554505bcee92768d8b98607054a77bd9
lineJeong <line.jeong32@gmail.com>
2023-01-27 18:57:10 +0900

    8-2. createContext와 Provider

--- 8.지뢰찾기/Form.jsx
+++ 8.지뢰찾기/Form.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useContext } from "react";
+import { START_GAME, tableContext } from "./MineSearch";
 
 function Form() {
   const [row, setRow] = useState(10);
   const [cell, setCell] = useState(10);
   const [mine, setMine] = useState(20);
+  const { dispatch } = useContext(tableContext);
 
   const onChangeRow = useCallback((e) => {
     setRow(e.target.value);
@@ -17,7 +19,9 @@ function Form() {
     setMine(e.target.value);
   }, []);
 
-  const onClickBtn = useCallback(() => {}, []);
+  const onClickBtn = useCallback(() => {
+    dispatch({ type: START_GAME, row, cell, mine });
+  }, [row, cell, mine]);
 
   return (
     <div>
--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -1,6 +1,22 @@
-import React, { useReducer } from "react";
+import React, { useReducer, createContext, useMemo } from "react";
 import Table from "./Table";
 import Form from "./Form";
+
+export const CODE = {
+  MINE: -7,
+  NORMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0, // 0 이상이면 opened
+};
+
+export const tableContext = createContext({
+  tableData: [],
+  dispatch: () => {},
+});
 
 const initialState = {
   tableData: [],
@@ -8,8 +24,16 @@ const initialState = {
   result: "",
 };
 
+export const START_GAME = "START_GAME";
+
 const reducer = (state, action) => {
   switch (action.type) {
+    case START_GAME:
+      return {
+        ...state,
+        tableData: plantMine(action.row, action.cell, action.mine),
+      };
+
     default:
       return state;
   }
@@ -18,13 +42,21 @@ const reducer = (state, action) => {
 const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const value = useMemo(
+    () => ({
+      tableData: state.tableData,
+      dispatch,
+    }),
+    [state.tableData]
+  );
+
   return (
-    <>
+    <tableContext.Provider value={value}>
       <Form />
       <div>{state.timer}</div>
       <Table />
       <div>{state.result}</div>
-    </>
+    </tableContext.Provider>
   );
 };
 
