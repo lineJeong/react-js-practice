58bd3cf03557e7f47c98b3722192f03f7caeae50
lineJeong <line.jeong32@gmail.com>
2023-02-07 12:49:43 +0900

    16. useEffect

--- velopert-modern-react/src/components/UserList.js
+++ velopert-modern-react/src/components/UserList.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
+  // useEffect 안에서 사용하는 상태나, props 가 있다면, useEffect 의 deps 에 넣어주어야 함
+  // 그렇지 않으면 useEffect 에 등록한 함수가 실행 될 때 최신 props / 상태를 가르키지 않음
+  useEffect(() => {
+    console.log("user 값이 설정됨");
+    console.log(user);
+    return () => {
+      console.log("user가 바뀌기 전");
+      console.log(user);
+    };
+  }, [user]);
+
   const style = {
     cursor: "pointer",
     color: user.active ? "green" : "black",
