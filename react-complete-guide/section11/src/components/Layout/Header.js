5dff3496f703043245f5b335c5b4cd956278216d
lineJeong <line.jeong32@gmail.com>
2023-03-04 23:08:17 +0900

    143. 장바구니 버튼 컴포넌트 추가하기

--- react-complete-guide/section11/src/components/Layout/Header.js
+++ react-complete-guide/section11/src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 function Header() {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food" />
