25cb7dcda5b07804f27a1fe2dc262290bfd4db7f
lineJeong <line.jeong32@gmail.com>
2023-03-06 17:59:28 +0900

    158. useEffect 훅 사용하기

--- react-complete-guide/section11/src/components/Layout/HeaderCartButton.js
+++ react-complete-guide/section11/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import useCart from "../../store/useCart";
+import { useEffect, useState } from "react";
 
 function HeaderCartButton(props) {
-  const cartCtx = useCart();
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+  const { items } = useCart();
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
