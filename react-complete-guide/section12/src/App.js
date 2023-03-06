4bf72964c6e5400cb25e92a8ed92ad454e99f731
lineJeong <line.jeong32@gmail.com>
2023-03-06 20:42:28 +0900

    165. useCallback()으로 함수 재생성 방지하기

--- react-complete-guide/section12/src/App.js
+++ react-complete-guide/section12/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
@@ -9,9 +9,9 @@ function App() {
 
   console.log("APP RUNNING");
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prev) => !prev);
-  };
+  }, []);
 
   // 가상 스냅샷 간(변경 전, 후)의 차이점만 Real DOM에 반영
   // real DOM에서는 DemoOutput의 <p> 요소만 변경되지만, App 컴포넌트에서 상태 관리를 하고 있기 때문에 App 컴포넌트도 재실행, 재평가가 발생함
