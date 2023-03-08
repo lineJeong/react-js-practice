6be02b18f2016c9b87d3c2ae85fa6ff5b703b6b3
lineJeong <line.jeong32@gmail.com>
2023-03-08 23:50:29 +0900

    192. POST 요청 보내기

--- react-complete-guide/section14/src/App.js
+++ react-complete-guide/section14/src/App.js
@@ -23,13 +23,22 @@ function App() {
 
       const data = await res.json();
 
-      const transformedMovies = data.results.map((movieData) => ({
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_data,
-        openingText: movieData.opening_crawl,
-      }));
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          releaseDate: data[key].releaseDate,
+          openingText: data[key].openingText,
+        });
+      }
+      // const transformedMovies = data.results.map((movieData) => ({
+      //   id: movieData.episode_id,
+      //   title: movieData.title,
+      //   releaseDate: movieData.release_data,
+      //   openingText: movieData.opening_crawl,
+      // }));
+      setMovies(loadedMovies);
     } catch (err) {
       setError(err.message);
     }
@@ -40,6 +49,34 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  const addMovieHandler = async (movie) => {
+    try {
+      const res = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}/movies.json`,
+        {
+          method: "POST",
+          body: JSON.stringify(movie),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!res.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await res.json();
+
+      const addedMovie = {
+        id: data.name,
+        ...movie,
+      };
+      setMovies((prevMovies) => [...prevMovies, addedMovie]);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   let content = <p>Found no movies.</p>;
   if (movies.length > 0) content = <MoviesList movies={movies} />;
   if (error) content = <p>{error}</p>;
@@ -48,7 +85,7 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <AddMovie />
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
