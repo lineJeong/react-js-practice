24ee9b93f75d33565cfa71da77c76792346fb035
lineJeong <line.jeong32@gmail.com>
2023-03-11 22:45:11 +0900

    221. 우리의 훅과 지식을 새로운 형태에 적용하기

--- react-complete-guide/section16/src/components/BasicForm.js
+++ react-complete-guide/section16/src/components/BasicForm.js
@@ -1,22 +1,102 @@
+import useInput from "../hooks/useInput";
+import Input from "./UI/Input";
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
+
 const BasicForm = (props) => {
+  const {
+    value: firstNameValue,
+    isValid: firstNameIsValid,
+    hasError: firstNameHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = useInput(isNotEmpty);
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+  } = useInput(isNotEmpty);
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
+
+  let formIsValid = false;
+  if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+    console.log(firstNameValue);
+    console.log(lastNameValue);
+    console.log(emailValue);
+
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+  };
+
+  const firstNameClasses = firstNameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const lastNameClasses = lastNameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const emailClasses = emailHasError ? "form-control invalid" : "form-control";
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
-        </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
-        </div>
+    <form onSubmit={formSubmissionHandler}>
+      <div className="control-group">
+        <Input
+          inputClasses={firstNameClasses}
+          id="firstName"
+          label="First Name"
+          type="text"
+          value={firstNameValue}
+          onChange={firstNameChangeHandler}
+          onBlur={firstNameBlurHandler}
+          hasError={firstNameHasError}
+          errorMsg="First Name must not be empty"
+        />
+        <Input
+          inputClasses={lastNameClasses}
+          id="lastName"
+          label="Last Name"
+          type="text"
+          value={lastNameValue}
+          onChange={lastNameChangeHandler}
+          onBlur={lastNameBlurHandler}
+          hasError={lastNameHasError}
+          errorMsg="Last Name must not be empty"
+        />
+        <Input
+          inputClasses={emailClasses}
+          id="email"
+          label="E-Mail Address"
+          type="email"
+          value={emailValue}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          hasError={emailHasError}
+          errorMsg="Email must include '@'"
+        />
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
-      </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
