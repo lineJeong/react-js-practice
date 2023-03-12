10b900c7e8931c8396e0915148097893ee762064
lineJeong <line.jeong32@gmail.com>
2023-03-12 21:00:54 +0900

    227. Http를 통해 식사 가져오기

---
+++ react-complete-guide/section17/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const res = await fetch(`${process.env.REACT_APP_SERVER_URL}/meals.json`);
+      const data = await res.json();
+
+      const loadedMeals = [];
+      for (let key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
