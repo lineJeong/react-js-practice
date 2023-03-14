5f8c37cc3f94d2ce4c8abc02ec97240b557d41fd
lineJeong <line.jeong32@gmail.com>
2023-03-14 17:21:09 +0900

    256. 다중 슬라이스 작업하기

--- react-complete-guide/section18/src/components/Auth.js
+++ react-complete-guide/section18/src/components/Auth.js
@@ -1,17 +1,44 @@
-import classes from './Auth.module.css';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { authActions } from "../store";
+
+import classes from "./Auth.module.css";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+  const [loginInputs, setLoginInputs] = useState({ email: "", password: "" });
+
+  const loginInputsChangeHandler = (event) => {
+    const [id, value] = event.target;
+    setLoginInputs((prev) => ({ ...prev, [id]: value }));
+  };
+
+  const loginSubmissionHandler = (event) => {
+    event.preventDefault();
+    dispatch(authActions.login());
+  };
+
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={loginSubmissionHandler}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={loginInputs.email}
+              onChange={loginInputsChangeHandler}
+            />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={loginInputs.password}
+              onChange={loginInputsChangeHandler}
+            />
           </div>
           <button>Login</button>
         </form>
--- react-complete-guide/section18/src/store/index.js
+++ react-complete-guide/section18/src/store/index.js
@@ -1,12 +1,11 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 // import { createStore } from "redux";
 
-const initialState = { counter: 0, showCounter: true };
-
 // redux-toolkit (createSlice)
+const initialCounterState = { counter: 0, showCounter: true };
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -23,14 +22,32 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
-  //   reducer: { counter: counterSlice.reducer },
+  //   reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 // redux (reducer function)
+// const initialState = { counter: 0, showCounter: true };
 // const counterReducer = (state = initialState, action) => {
 //   switch (action.type) {
 //     case "increment":
