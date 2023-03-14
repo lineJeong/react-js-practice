87df8e3df50708afa67b1ce7dfbbbdad2d1c4918
lineJeong <line.jeong32@gmail.com>
2023-03-14 16:18:03 +0900

    254. 리덕스 툴킷 State 연결하기

--- react-complete-guide/section18/src/store/index.js
+++ react-complete-guide/section18/src/store/index.js
@@ -1,9 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
 
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+// redux-toolkit (createSlice)
+const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
@@ -22,19 +23,25 @@ createSlice({
   },
 });
 
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "increment":
-      return { ...state, counter: state.counter + action.amount };
-    case "decrement":
-      return { ...state, counter: state.counter - 1 };
-    case "toggle":
-      return { ...state, showCounter: !state.showCounter };
-    default:
-      return state;
-  }
-};
+const store = configureStore({
+  reducer: counterSlice.reducer,
+  //   reducer: { counter: counterSlice.reducer },
+});
 
-const store = createStore(counterReducer);
+// redux (reducer function)
+// const counterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return { ...state, counter: state.counter + action.amount };
+//     case "decrement":
+//       return { ...state, counter: state.counter - 1 };
+//     case "toggle":
+//       return { ...state, showCounter: !state.showCounter };
+//     default:
+//       return state;
+//   }
+// };
+
+// const store = createStore(counterReducer);
 
 export default store;
