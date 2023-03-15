3437e871d237b92658bf7b9535ebf6611c96e614
lineJeong <line.jeong32@gmail.com>
2023-03-15 19:30:57 +0900

    325. 인증 작업 실행하기

--- react-complete-guide/section21/frontend/src/App.js
+++ react-complete-guide/section21/frontend/src/App.js
@@ -13,7 +13,7 @@ import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import Authentication from "./pages/Authentication";
+import Authentication, { action as authAction } from "./pages/Authentication";
 
 const router = createBrowserRouter([
   {
@@ -63,6 +63,7 @@ const router = createBrowserRouter([
       {
         path: "auth",
         element: <Authentication />,
+        action: authAction,
       },
     ],
   },
