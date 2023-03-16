fdc34fe685ded105f8688e92035cea163dfb2803
lineJeong <line.jeong32@gmail.com>
2023-03-16 19:02:08 +0900

    334. 토큰 만료 관리하기

--- react-complete-guide/section21/frontend/src/util/auth.js
+++ react-complete-guide/section21/frontend/src/util/auth.js
@@ -1,7 +1,24 @@
 import { redirect } from "react-router-dom";
 
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationData = new Date(storedExpirationDate);
+  const now = new Date();
+  const duration = expirationData.getTime() - now.getTime();
+  return duration;
+}
+
 export function getAuthToken() {
   const token = localStorage.getItem("token");
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
   return token;
 }
 
@@ -21,4 +38,5 @@ export function checkAuthPageLoader() {
   if (token) {
     return redirect("/");
   }
+  return null;
 }
