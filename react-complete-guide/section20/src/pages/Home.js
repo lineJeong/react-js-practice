a35c3b94e7b134ef8d1ee130ed10f24e7e9621a0
lineJeong <line.jeong32@gmail.com>
2023-03-19 23:17:51 +0900

    283. Link로 페이지들 간에 이동하기

--- react-complete-guide/section20/src/pages/Home.js
+++ react-complete-guide/section20/src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <h1>HomePage</h1>
+      <p>
+        Go to <Link to="/products">pruducts</Link>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
