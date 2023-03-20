26e77a5fcc5d6b26d2331b2e496ea70386e52c28
lineJeong <line.jeong32@gmail.com>
2023-03-20 17:08:11 +0900

    287. 프로그램적으로 네비게이션하기

--- react-complete-guide/section20/src/pages/Home.js
+++ react-complete-guide/section20/src/pages/Home.js
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("/products");
+  };
+
   return (
     <>
       <h1>HomePage</h1>
       <p>
         Go to <Link to="/products">pruducts</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
