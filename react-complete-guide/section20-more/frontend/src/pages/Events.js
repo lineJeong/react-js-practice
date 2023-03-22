960cb2168cc7cbdb7cb592b71e0761893d4d8c23
lineJeong <line.jeong32@gmail.com>
2023-03-22 17:00:48 +0900

    295. loader()를 이용한 데이터 가져오기

--- react-complete-guide/section20-more/frontend/src/pages/Events.js
+++ react-complete-guide/section20-more/frontend/src/pages/Events.js
@@ -1,37 +1,35 @@
+import { useEffect, useState } from "react";
+
 import EventsList from "../components/EventsList";
 
-const EVENTS = [
-  {
-    id: "1",
-    image:
-      "https://images.unsplash.com/photo-1617854818583-09e7f077a156?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80​",
-    title: "TITLE 1",
-    date: "2022-03-22",
-    description: "description 1",
-  },
-  {
-    id: "2",
-    image:
-      "https://images.unsplash.com/photo-1587691592099-24045742c181?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80​",
-    title: "TITLE 2",
-    date: "2022-03-22",
-    description: "description 2",
-  },
-  {
-    id: "3",
-    image:
-      "https://images.unsplash.com/photo-1624555130581-1d9cca783bc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80​",
-    title: "TITLE 3",
-    date: "2022-03-22",
-    description: "description 3",
-  },
-];
-
 function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/events");
+
+      if (!response.ok) {
+        setError("Fetching events failed.");
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
   return (
     <>
-      <h1>EventsPage</h1>
-      <EventsList events={EVENTS} />
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
     </>
   );
 }
