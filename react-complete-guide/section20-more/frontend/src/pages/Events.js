0ec95dd9d61dd62be376ebda4c1476be248ea068
lineJeong <line.jeong32@gmail.com>
2023-03-23 21:49:24 +0900

    301. loader()에서 응답 리턴하기

--- react-complete-guide/section20-more/frontend/src/pages/Events.js
+++ react-complete-guide/section20-more/frontend/src/pages/Events.js
@@ -2,7 +2,8 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return (
     <>
@@ -17,9 +18,8 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // setError("Fetching events failed.");
+    // ...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
