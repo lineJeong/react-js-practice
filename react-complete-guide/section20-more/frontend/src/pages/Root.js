7f4b5108e9a090c7207fbcd147b9dc67fd2e233b
lineJeong <line.jeong32@gmail.com>
2023-03-23 21:36:17 +0900

    300. 현재 네비게이션 상태를 UI에 반영하기

--- react-complete-guide/section20-more/frontend/src/pages/Root.js
+++ react-complete-guide/section20-more/frontend/src/pages/Root.js
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
+// import { useNavigation } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 
 function RootLayout() {
+  // const navigatiton = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navigatiton.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
