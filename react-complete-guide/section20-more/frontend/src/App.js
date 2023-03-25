01062712783992ceed7ad97354b2c6a518b171c4
lineJeong <line.jeong32@gmail.com>
2023-03-25 17:52:58 +0900

    309. action() 함수 사용하기

--- react-complete-guide/section20-more/frontend/src/App.js
+++ react-complete-guide/section20-more/frontend/src/App.js
@@ -8,7 +8,7 @@ import EventDetailPage, {
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
@@ -45,6 +45,7 @@ const router = createBrowserRouter([
           {
             path: "new",
             element: <NewEventPage />,
+            action: newEventAction,
           },
         ],
       },
