053c3debc62c649c0bd9e5d326ad47ccb6d8d3ae
lineJeong <line.jeong32@gmail.com>
2023-04-07 16:05:21 +0900

    1. 프로젝트 준비 및 기본적인 사용법

---
+++ velopert-modern-react/section5/src/App.js
@@ -0,0 +1,25 @@
+import { Link, Routes, Route } from "react-router-dom";
+
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
