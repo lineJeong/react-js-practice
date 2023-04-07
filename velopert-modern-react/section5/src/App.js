6deb89c552e074024e5268fe7306b250b292d0c0
lineJeong <line.jeong32@gmail.com>
2023-04-07 16:36:18 +0900

    3. 서브라우트

--- velopert-modern-react/section5/src/App.js
+++ velopert-modern-react/section5/src/App.js
@@ -2,7 +2,7 @@ import { Link, Routes, Route } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+import Profiles from "./pages/Profiles";
 
 function App() {
   return (
@@ -14,11 +14,14 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
-        <Route path="/profiles/:username" element={<Profile />} />
+        <Route path="/profiles/*" element={<Profiles />} />
       </Routes>
     </>
   );
---
+++ velopert-modern-react/section5/src/pages/Profiles.js
@@ -0,0 +1,24 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Profile from "./Profile";
+
+function Profiles() {
+  return (
+    <>
+      <h3>유저 목록:</h3>
+      <ul>
+        <li>
+          <Link to="/profiles/velopert">velopert</Link>
+        </li>
+        <li>
+          <Link to="/profiles/gildong">gildong</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route index element="유저를 선택해주세요" />
+        <Route path=":username" element={<Profile />} />
+      </Routes>
+    </>
+  );
+}
+
+export default Profiles;
