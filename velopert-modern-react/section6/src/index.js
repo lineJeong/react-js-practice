07d9204c77762e1d756278655dcfdded588b23ca
lineJeong <line.jeong32@gmail.com>
2023-04-10 17:07:12 +0900

    6. 리덕스 개발자도구 적용하기

--- velopert-modern-react/section6/src/index.js
+++ velopert-modern-react/section6/src/index.js
@@ -6,8 +6,9 @@ import App from "./App";
 import rootReducer from "./modules/index";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
