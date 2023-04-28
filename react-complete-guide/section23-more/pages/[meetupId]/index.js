5551a3031fcb2578c5996ba7c17acaa57bcdffda
lineJeong <line.jeong32@gmail.com>
2023-04-28 23:27:39 +0900

    368. getStaticPaths로 경로 준비 및 대체 페이지 작업하기

--- react-complete-guide/section23-more/pages/[meetupId]/index.js
+++ react-complete-guide/section23-more/pages/[meetupId]/index.js
@@ -11,6 +11,24 @@ function MeetupDetails(props) {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
 export async function getStaticProps(context) {
   //fetch data form a single meetup
   const meetupId = context.params.meetupId;
