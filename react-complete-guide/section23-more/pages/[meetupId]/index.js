5d22cd6ec52bd1a71ddaf5c40a1ebd7df95ae4b4
lineJeong <line.jeong32@gmail.com>
2023-04-28 22:52:52 +0900

    367. SSG 데이터 가져오기를 위한 Params 작업하기

--- react-complete-guide/section23-more/pages/[meetupId]/index.js
+++ react-complete-guide/section23-more/pages/[meetupId]/index.js
@@ -1,6 +1,6 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
       image="https://plus.unsplash.com/premium_photo-1682192408126-70498c5f488b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1732&q=80"
@@ -9,6 +9,24 @@ function MeetupDetails() {
       description="description1"
     />
   );
+}
+
+export async function getStaticProps(context) {
+  //fetch data form a single meetup
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://plus.unsplash.com/premium_photo-1682192408126-70498c5f488b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1732&q=80",
+        title: "title1",
+        address: "address1",
+        description: "description1",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
