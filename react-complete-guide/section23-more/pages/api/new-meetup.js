c304689ac2fae6992decb0178089e98a15e28c20
lineJeong <line.jeong32@gmail.com>
2023-04-29 17:47:03 +0900

    370. 몽고DB로 작업하기

--- react-complete-guide/section23-more/pages/api/new-meetup.js
+++ react-complete-guide/section23-more/pages/api/new-meetup.js
@@ -1,11 +1,25 @@
 // /api/new-meetup
 // POST /api/new-meetup
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
+  const { mongoUsername, mongoPassword } = process.env;
+
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      `mongodb+srv://${mongoUsername}:${mongoPassword}@boiler-plate.azbi4mt.mongodb.net/meetups?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
