2a5f227756600949aaaa6d387cc4674336640c6c
lineJeong <line.jeong32@gmail.com>
2023-04-29 17:04:33 +0900

    369. API 경로 소개

--- react-complete-guide/section23-more/pages/[meetupId]/index.js
+++ react-complete-guide/section23-more/pages/[meetupId]/index.js
@@ -3,10 +3,10 @@ import MeetupDetail from "../../components/meetups/MeetupDetail";
 function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://plus.unsplash.com/premium_photo-1682192408126-70498c5f488b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1732&q=80"
-      title="title1"
-      address="address1"
-      description="description1"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
