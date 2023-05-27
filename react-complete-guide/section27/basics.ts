bc54e23bf3feb78088ef5b48a6cf1355ce5092ef
lineJeong <line.jeong32@gmail.com>
2023-05-27 19:38:59 +0900

    428. 배열 및 객체 유형 작업하기

--- react-complete-guide/section27/basics.ts
+++ react-complete-guide/section27/basics.ts
@@ -9,3 +9,22 @@ userName = "Max";
 let isInstructor: boolean;
 
 isInstructor = true;
+
+let hobbies: string[];
+
+hobbies = ["Cooking", "Programming", "Riding"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
