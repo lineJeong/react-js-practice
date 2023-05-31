30588fc89b11b42e176ab842e2ccacb34ec5dafa
lineJeong <line.jeong32@gmail.com>
2023-05-31 15:29:12 +0900

    433. 제네릭으로 뛰어들다

--- react-complete-guide/section27/basics.ts
+++ react-complete-guide/section27/basics.ts
@@ -43,3 +43,17 @@ function add2(a: number, b: number) {
 function printOutput(value: any) {
   console.log(value);
 }
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1);
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");
+
+// updatedArray[0].split("");
+stringArray[0].split("");
